ba9c308f9a592c4888f9cfa8606a484181822ad1
Aniket <aniketraj2559@gmail.com>
2025-04-14 00:50:51 +0530

    merge isssue solve

--- TarkVitark/src/App.jsx
+++ TarkVitark/src/App.jsx
@@ -1,12 +1,10 @@
 
 import React from 'react'
-<<<<<<< HEAD
 
 import HomePage from './pages/HomePage'
 //import Navbar from 'react'
 import  Navbar from './components/Navbar'
  
-=======
 //import './App.css'
 import Landing from './pages/Landing'
 import LeftSideBar from './components/LeftSideBar'
@@ -16,17 +14,14 @@ import Login from './pages/login'
 import Register from './pages/Register'
 import ActiveDiscussion from './pages/ActiveDiscussion'
 import FutureEvents from './pages/FutureEvents'
->>>>>>> e99f7a73a3c75827b3a9e7745083e62c370ab655
 
 function App() {
   
 
   return (
     <>
-<<<<<<< HEAD
    
      <HomePage/>
-=======
      {/* <Landing/> */}
      {/* <LeftSideBar/> */}
      {/* <ProfilePage/> */}
@@ -36,7 +31,6 @@ function App() {
      <FutureEvents/>
      
 
->>>>>>> e99f7a73a3c75827b3a9e7745083e62c370ab655
      
     
     </>
